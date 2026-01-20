14ac93aba706569109a7631a81f38e8bc5d182f3
lewis-green <lewis.green@monitra.co.uk>
2026-01-20 14:55:43 +0000

    initial commit

---
+++ src/pd_selector.py
@@ -0,0 +1,10 @@
+"""
+Multi Measure Ensemble PD selector. 
+
+Combines multiple selection methods to robustly identify partial discharge events from clustered data. 
+
+Thresholds of each measure to be defined either from clustering statistics or domain knowledge.
+Likely a combination of both. 
+
+
+"""
