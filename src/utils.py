d36972ff242219325101c2200e22128199329552
lewis-green <lewis.green@monitra.co.uk>
2026-02-02 16:07:13 +0000

    added yamane sampling technique

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_har
 def plot_clusters(df):
     
     fig, ax = plt.subplots(figsize=(10, 6))
-    scatter = ax.scatter(df['observedTime_ms'], df['energy'], c=df['cluster'], cmap='tab20', s=10)
+    scatter = ax.scatter(df['observedTime_ms'], df['energy'], c=df['cluster'], cmap='rainbow', s=10)
     legend1 = ax.legend(*scatter.legend_elements(), title="Clusters")
     ax.add_artist(legend1)
     ax.set_title('Clustering Results')
@@ -39,3 +39,20 @@ def validate_clusters(df):
         print(f'Calinski-Harabasz Score: {ch_score:.4f}')
     else:
         print('Not enough clusters or features to compute validation metrics.')
+
+def sampleDataset(df, seed):
+    """
+    Get a subsample of the dataset. 
+    Uses Yamane simplified method to determine sample size.
+
+    """
+    if isinstance(df, pd.DataFrame ): # DataFrame (not reduced)
+         n_samples = len(df.index) / (1 + (len(df.index) * 0.01**2))
+         sample = df.sample(round(n_samples), random_state= seed)
+
+    else: # numpy array (pca reduced)
+         n_samples =  df[:,0].shape[0] / (1 +  df[:,0].shape[0] * 0.01**2) 
+         df = pd.DataFrame(df)
+         sample = df.sample(round(n_samples), random_state=seed)
+
+    return sample
